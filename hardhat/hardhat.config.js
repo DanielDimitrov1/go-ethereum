8da17852a3933c01b773d92ac85eb34c20f20773
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-14 19:40:06 +0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 const { vars } = require("hardhat/config");
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("2e0834786285daccd064ca17f1654f67b4aef298acbb82cef9ec422fb4975622");
 
 module.exports = {
