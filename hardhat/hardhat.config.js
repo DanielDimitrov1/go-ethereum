2bfdaad2c9936910d0f5067952b1fded39656507
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-14 21:25:14 +0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.13",
   networks: {
     localhost: {
-      url: `http://127.0.0.1:8545`,
+      url: `http://127.0.0.1:3500`,
       accounts: [DEVNET_PRIVATE_KEY],
     },
   },
