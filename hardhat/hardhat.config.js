9fa18ec06216f061e7c196ccad3b9955746f53f9
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-14 19:44:13 +0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 const { vars } = require("hardhat/config");
 
-const SEPOLIA_PRIVATE_KEY = vars.get("2e0834786285daccd064ca17f1654f67b4aef298acbb82cef9ec422fb4975622");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 module.exports = {
   solidity: "0.8.24",
