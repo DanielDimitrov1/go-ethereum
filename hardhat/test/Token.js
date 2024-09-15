76e0e7ed040385abd9a875fe582a44232f10b165
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-15 16:22:53 +0300

    Update Token.js

--- hardhat/test/Token.js
+++ hardhat/test/Token.js
@@ -1,12 +1,13 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
 
-    const hardhatToken = await ethers.deployContract("Token");
-
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    const ownerBalance = await token.balanceOf(owner.address);
+    expect(await token.totalSupply()).to.equal(ownerBalance);
   });
 });
