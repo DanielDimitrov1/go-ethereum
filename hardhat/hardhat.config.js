ad685bb5379e7e109c7b72c04af51f768f7af9df
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-15 16:10:50 +0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,15 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 const { vars } = require("hardhat/config");
 
-const DEVNET_PRIVATE_KEY = process.env.DEVNET_PRIVATE_KEY
+const DEVNET_PRIVATE_KEY = process.env.DEVNET_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.13",
   networks: {
     devnet: {
-      url: 'http://localhost:8545/${process.env.DEVNET_PRIVATE_KEY}',
-      accounts: [DEVNET_PRIVATE_KEY],
-      chainId: "32382"
+      url: 'http://localhost:8545',  // The RPC URL
+      accounts: DEVNET_PRIVATE_KEY ? [`0x${DEVNET_PRIVATE_KEY}`] : [], // Ensure the private key is prefixed with 0x and is properly passed
+      chainId: 32382,  // Use number for chainId
+      gas: 'auto',     // Optional: you can specify the gas limit
+      gasPrice: 'auto' // Optional: use 'auto' for default gas price
     },
   },
 };
