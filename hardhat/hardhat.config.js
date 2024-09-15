dcce1ff0138395b0e5965fead9802902a95e822f
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-16 00:15:49 +0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,7 @@ const DEVNET_PRIVATE_KEY = process.env.DEVNET_PRIVATE_KEY;
 
 module.exports = {
   solidity: {
-    version: "0.8.24",  // Updated to match the contract's Solidity version
+    version: "0.8.24",  
     settings: {
       optimizer: {
         enabled: true,
@@ -16,7 +16,7 @@ module.exports = {
   networks: {
     devnet: {
       url: 'http://localhost:8545',
-      accounts: DEVNET_PRIVATE_KEY ? [`0x${DEVNET_PRIVATE_KEY}`] : [`0x${DEVNET_PRIVATE_KEY}`],
+      accounts: [`0x${DEVNET_PRIVATE_KEY}`],
       chainId: 32382,
       gas: 'auto',
       gasPrice: 'auto',
