b088030d894b31396ccf25363e1cb2e742150c75
Daniel Dimitrov <112561988+DanielDimitrov1@users.noreply.github.com>
2024-09-15 16:42:34 +0300

    Update Token.sol

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -1,1 +1,7 @@
-0x4242424242424242424242424242424242424242 
+// contracts/Token.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    0x4242424242424242424242424242424242424242
+}
